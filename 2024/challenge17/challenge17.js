cbb2794e095a20358af385d896f56fa8a119a606
Habakuk Beneke <habakukbeneke@proton.me>
2025-01-03 10:16:54 +0100

    Challenge 17

---
+++ 2024/challenge17/challenge17.js
@@ -0,0 +1,41 @@
+function detectBombs(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  const directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  const result = Array.from({ length: rows }, () => Array(cols).fill(0));
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      let bombCount = 0;
+      for (const [dx, dy] of directions) {
+        const newRow = row + dx;
+        const newCol = col + dy;
+
+        if (
+          newRow >= 0 &&
+          newRow < rows &&
+          newCol >= 0 &&
+          newCol < cols &&
+          grid[newRow][newCol]
+        ) {
+          bombCount++;
+        }
+      }
+
+      result[row][col] = bombCount;
+    }
+  }
+
+  return result;
+}
